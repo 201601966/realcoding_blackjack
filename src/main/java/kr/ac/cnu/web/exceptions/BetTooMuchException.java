2c1cf613fad098c950b78b81e559e0c0bd400690
201602064 <201602064@cs-cnu.org>
2018-06-18 13:49:41 +0900

    fixed#8

---
+++ src/main/java/kr/ac/cnu/web/exceptions/BetTooMuchException.java
@@ -0,0 +1,9 @@
+package kr.ac.cnu.web.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+public class BetTooMuchException extends RuntimeException {
+
+}
